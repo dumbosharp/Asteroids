b8861ccc2fb29e1ea74cf8085c2ba835b8b0eafb
dumbosharp <bolingjoshua@gmail.com>
2025-04-26 17:35:05 -0400

    we have a player triangle that can now move around the whole screen

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
-        pygame.Surface.fill(screen, (0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
 
 
